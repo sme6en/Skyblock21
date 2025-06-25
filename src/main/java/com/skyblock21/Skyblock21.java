010087f8dbdf1aad84b916bb4830d761c6507741
sme6en <sme6en@protonmail.com>
2025-06-25 18:09:47 +0300

    chore: bump version to 1.2.2

--- src/main/java/com/skyblock21/Skyblock21.java
+++ src/main/java/com/skyblock21/Skyblock21.java
@@ -39,7 +39,7 @@ import static net.fabricmc.fabric.api.client.command.v2.ClientCommandManager.lit
 
 public class Skyblock21 implements ClientModInitializer {
     public static final String MOD_ID = "skyblock21";
-    public static final String MOD_VERSION = "1.2.1";
+    public static final String MOD_VERSION = "1.2.2";
     public static final Gson GSON = new GsonBuilder().setPrettyPrinting().create();
 
 
